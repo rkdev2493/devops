443ec42ded4419bafe1cdee456f8e02db94312d0
rajesh kumar <rajeechoudhary786@gmail.com>
2025-07-13 20:42:49 +0530

    updated smm from list to string

--- modules/ec2/ssm.tf
+++ modules/ec2/ssm.tf
@@ -5,10 +5,10 @@ resource "aws_ssm_association" "cloudwatch_agent" {
     values = [aws_instance.ec2_instance.id]
   }
   parameters = {
-    action = ["Install"]
-    mode   = ["ec2"]
-    optionalConfigurationSource = ["ssm"]
-    optionalConfigurationLocation = [aws_ssm_parameter.cloudwatch_agent_config.name]
-    optionalRestart = ["yes"]
+    action = "Install"
+    mode   = "ec2"
+    optionalConfigurationSource = "ssm"
+    optionalConfigurationLocation = aws_ssm_parameter.cloudwatch_agent_config.name
+    optionalRestart = "yes"
   }
 }
