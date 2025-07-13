2b7f771f39a68c0214cfcc85e05a3e4eab9d7454
rajesh kumar <rajeechoudhary786@gmail.com>
2025-07-13 21:59:18 +0530

    ssm

--- modules/ec2/ssm.tf
+++ modules/ec2/ssm.tf
@@ -1,15 +1,18 @@
 resource "aws_ssm_association" "cloudwatch_agent" {
   name = "AmazonCloudWatch-ManageAgent"
+
   targets {
     key    = "InstanceIds"
     values = [aws_instance.ec2_instance.id]
   }
+
   parameters = {
-    action = "configure"
-    mode   = "ec2"
-    optionalConfigurationSource = "ssm"
-    optionalConfigurationLocation = aws_ssm_parameter.cloudwatch_agent_config.name
-    optionalRestart = "yes"
-    name = aws_instance.ec2_instance.id
+    action                      = ["Configure"]
+    mode                        = ["ec2"]
+    optionalConfigurationSource = ["ssm"]
+    optionalConfigurationLocation = [aws_ssm_parameter.cloudwatch_agent_config.name]
+    optionalRestart             = ["yes"]
   }
+
+  depends_on = [aws_instance.ec2_instance, aws_ssm_parameter.cloudwatch_agent_config]
 }
